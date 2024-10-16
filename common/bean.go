3df5186e594c57cb4fe10c3237a19eec59e29dfa
kartik-579 <kartik@devtron.ai>
2024-10-16 13:34:32 +0530

    wip

--- common/bean.go
+++ common/bean.go
@@ -17,6 +17,7 @@
 package common
 
 import (
+	git "github.com/devtron-labs/common-lib/git-manager"
 	"github.com/optiopay/klar/clair"
 	"github.com/quay/claircore"
 	"strings"
@@ -67,27 +68,27 @@ type ImageScanRenderDto struct {
 }
 
 type ImageScanEvent struct {
-	Image            string `json:"image"`
-	ImageDigest      string `json:"imageDigest"`
-	AppId            int    `json:"appId"`
-	EnvId            int    `json:"envId"`
-	PipelineId       int    `json:"pipelineId"`
-	CiArtifactId     int    `json:"ciArtifactId"`
-	UserId           int    `json:"userId"`
-	AccessKey        string `json:"accessKey"`
-	SecretKey        string `json:"secretKey"`
-	Token            string `json:"token"`
-	AwsRegion        string `json:"awsRegion"`
-	DockerRegistryId string `json:"dockerRegistryId"`
-	DockerConnection string `json:"dockerConnection"`
-	DockerCert       string `json:"dockerCert"`
-	//CiProjectDetails []helper.CiProjectDetails `json:"ciProjectDetails"`
-	SourceType     SourceType    `json:"sourceType"`
-	SourceSubType  SourceSubType `json:"sourceSubType"`
-	CiWorkflowId   int           `json:"ciWorkflowId"`
-	CdWorkflowId   int           `json:"cdWorkflowId"`
-	ChartHistoryId int           `json:"chartHistoryId"`
-	ManifestData   *ManifestData `json:"manifestData"`
+	Image            string                 `json:"image"`
+	ImageDigest      string                 `json:"imageDigest"`
+	AppId            int                    `json:"appId"`
+	EnvId            int                    `json:"envId"`
+	PipelineId       int                    `json:"pipelineId"`
+	CiArtifactId     int                    `json:"ciArtifactId"`
+	UserId           int                    `json:"userId"`
+	AccessKey        string                 `json:"accessKey"`
+	SecretKey        string                 `json:"secretKey"`
+	Token            string                 `json:"token"`
+	AwsRegion        string                 `json:"awsRegion"`
+	DockerRegistryId string                 `json:"dockerRegistryId"`
+	DockerConnection string                 `json:"dockerConnection"`
+	DockerCert       string                 `json:"dockerCert"`
+	CiProjectDetails []git.CiProjectDetails `json:"ciProjectDetails"`
+	SourceType       SourceType             `json:"sourceType"`
+	SourceSubType    SourceSubType          `json:"sourceSubType"`
+	CiWorkflowId     int                    `json:"ciWorkflowId"`
+	CdWorkflowId     int                    `json:"cdWorkflowId"`
+	ChartHistoryId   int                    `json:"chartHistoryId"`
+	ManifestData     *ManifestData          `json:"manifestData"`
 }
 
 type ScanEventResponse struct {
